73f42990780b3484d85a1d0a63f8d209d43bd4e7
Juliana <julianabandure@gmail.com>
2021-12-14 14:31:20 -0300

    confirmar

--- angular-app/src/app/formularios/meu-form/meu-form.component.ts
+++ angular-app/src/app/formularios/meu-form/meu-form.component.ts
@@ -1,51 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-meu-form',
   templateUrl: './meu-form.component.html',
-  styleUrls: ['./meu-form.component.css'],
+  styleUrls: ['./meu-form.component.css']
 })
 export class MeuFormComponent implements OnInit {
-  meuForm: FormGroup = new FormGroup({
-    email: new FormControl(),
-    password: new FormControl(),
-    checkme: new FormControl(),
-  });
 
-  constructor(private fb: FormBuilder) {}
+  meuForm : FormGroup = new FormGroup(
+    {
+      email : new FormControl(),
+      nome : new FormControl(),
+      checkme : new FormControl()
+    }
+  );
 
-  ngOnInit(): void {
-    this.meuForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(8),
-          Validators.pattern(
-            '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$'
-          ),
-        ],
-      ],
-      checkme: ['', []],
-    });
-    console.log(this.meuForm);
+  constructor(private fb : FormBuilder) {
   }
 
-  getControl(control: string) {
+  ngOnInit(): void {
+
+    this.meuForm = this.fb.group({
+      email : [ '' , [ Validators.required, Validators.minLength(5), Validators.email ] ],
+      nome : [ '', [ Validators.required ]],
+      checkme : [ '', [] ]
+    });
+    console.log(this.meuForm)
+  }
+
+  getControl(control : string){
     return this.meuForm.get(control);
   }
 
-  isValid(control: string) {
-    return (
-      this.getControl(control)?.valid == false &&
-      this.getControl(control)?.touched
-    );
+  isValid(control :string){
+    return (this.getControl(control)?.valid == false && this.getControl(control)?.touched)
   }
 }
